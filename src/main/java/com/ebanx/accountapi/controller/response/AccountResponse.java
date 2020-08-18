58c4a3dc4f5f906b22e69649b5bb7bc42d99fe36
Wesley Cruz <wesley.cruz@mobi7.com.br>
2020-08-18 20:38:29 -0300

    Added notation to ignore properties that's null

--- src/main/java/com/ebanx/accountapi/controller/response/AccountResponse.java
+++ src/main/java/com/ebanx/accountapi/controller/response/AccountResponse.java
@@ -1,13 +1,13 @@
 package com.ebanx.accountapi.controller.response;
 
 import com.ebanx.accountapi.entity.Account;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-@JsonIgnoreProperties
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class AccountResponse {
     private final Account origin;
     private final Account destination;
