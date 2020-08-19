ae32919237c510b8fc07dc6d431761c209b37e99
Wesley Cruz <wesley.cruz@mobi7.com.br>
2020-08-18 22:16:32 -0300

    Removed unused files, improved logs and organize methods

--- src/main/java/com/ebanx/accountapi/service/AccountService.java
+++ src/main/java/com/ebanx/accountapi/service/AccountService.java
@@ -13,53 +13,20 @@ import java.util.*;
 public class AccountService {
     private final List<Account> accounts = new ArrayList<>();
 
-    public Account withdraw(String originId, BigDecimal amount) throws NoSuchElementException {
-        Account acc = getAccount(originId);
-        BigDecimal newBalance = acc.getBalance().subtract(amount);
-        acc.setBalance(newBalance);
-        return acc;
-    }
-
-    public Account deposit(String id, BigDecimal amount) {
-        try {
-            Account acc = getAccount(id);
-            BigDecimal newBalance = amount.add(acc.getBalance());
-            acc.setBalance(newBalance);
-            return acc;
-        } catch (NoSuchElementException e) {
-            return createAccount(id, amount);
-        }
-    }
-
-    public Account getAccount(String id) {
-        log.info("Finding accountId: " + id);
-        return accounts.stream()
-                .filter(account -> account.getId().equals(id))
-                .findFirst()
-                .orElseThrow(NoSuchElementException::new);
-    }
-
-    public Account createAccount(String accountId, BigDecimal amount) throws NoSuchElementException {
-        log.info("Creating an account with id: " + accountId);
-        Account acc = Account.builder()
-                .id(accountId)
-                .balance(amount)
-                .build();
-
-        this.accounts.add(acc);
-        return getAccount(acc.getId());
-    }
-
-    public BigDecimal getBalance(String accountId) throws NoSuchElementException {
-        return getAccount(accountId).getBalance();
-    }
-
     public void reset() {
         log.info("Clearing accounts...");
         this.accounts.clear();
     }
 
+    public BigDecimal getBalance(String accountId) throws NoSuchElementException {
+        log.info("Get Balance from accountId: " + accountId);
+        return getAccount(accountId).getBalance();
+    }
+
     public Map<String, Account> transfer(AccountRequest request) throws NoSuchElementException{
+        log.info("Transfer: " + request.getAmount() + "from account: "
+                + request.getOrigin() + "to account: " + request.getDestination());
+
         Map<String, Account> map = new HashMap<>();
         Account origin = withdraw(request.getOrigin(), request.getAmount());
         Account destination = deposit(request.getDestination(), request.getAmount());
@@ -69,4 +36,46 @@ public class AccountService {
 
         return map;
     }
+
+    public Account withdraw(String originAccountId, BigDecimal amount) throws NoSuchElementException {
+        log.info("Withdrawing: " + amount + "from account: " + originAccountId);
+        Account acc = getAccount(originAccountId);
+        BigDecimal newBalance = acc.getBalance().subtract(amount);
+        acc.setBalance(newBalance);
+        return acc;
+    }
+
+    public Account deposit(String destinationAccountId, BigDecimal amount) {
+        log.info("Depositing: " + amount + "to account: " + destinationAccountId);
+        try {
+            Account acc = getAccount(destinationAccountId);
+            BigDecimal newBalance = amount.add(acc.getBalance());
+            acc.setBalance(newBalance);
+            return acc;
+        } catch (NoSuchElementException e) {
+            return createAccount(destinationAccountId, amount);
+        }
+    }
+
+    private Account createAccount(String accountId, BigDecimal amount) throws NoSuchElementException {
+        log.info("Creating an account with id: " + accountId);
+
+        Account acc = Account.builder()
+                .id(accountId)
+                .balance(amount)
+                .build();
+
+        this.accounts.add(acc);
+        return getAccount(acc.getId());
+    }
+
+    private Account getAccount(String accountId) {
+        log.info("Finding accountId: " + accountId);
+
+        return accounts.stream()
+                .filter(account -> account.getId().equals(accountId))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
+    }
+
 }
